f6c1ac75ae752477cb7a47a30067a5e0ee63e906
LPII <Lanceportergd@gmail.com>
2016-09-16 22:38:47 -0400

    Add c1

---
+++ c1.lisp
@@ -0,0 +1,16 @@
+(* 5 99)
+(+ 21 35 12 7)
+;prefix notation , good for lots of arguments
+(* 25 4 12)
+;nested combination 
+(+ (* 3 5) (- 10 6))
+;var declaration
+(define age 27)
+age
+(* age 2)
+;functions
+(define (add a b)
+	(+ a b))
+
+(add 1 2)
+; ended at 1.1.6
